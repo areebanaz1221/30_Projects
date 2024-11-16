33dc1e6aba2e8c5a5fd4dd2f0681bfec65e5178e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-11-16 21:37:09 +0500

    Add files via upload

---
+++ 1_Countdown_Timer/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import CoundownTimer from '@/components/CountdownTimer';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <CoundownTimer />
+    </div>
+  )
+}
+
+export default Home; 
